bbff7f4ef6d1c4fc1198778a7fb93e018a1b8775
owen wilson <owenwilson814@yahoo.com>
2025-09-27 22:23:29 +0100

    changes

--- src/main/java/com/napier/devops/Main.java
+++ src/main/java/com/napier/devops/Main.java
@@ -8,7 +8,7 @@ public class Main
     public static void main(String[] args)
     {
 
-            MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
 
     }
 }
